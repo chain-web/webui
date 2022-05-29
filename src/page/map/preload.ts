5a9e19cd95d86b27e766bcbde1b741b3b4de3fa0
chengcheng <chengcheng@qcraft.ai>
2022-05-29 12:44:34 +0800

    add elements config

--- src/page/map/preload.ts
+++ src/page/map/preload.ts
@@ -1,49 +1,4 @@
 import { Map } from 'mapbox-gl';
-// import {
-//   MeshBasicMaterial,
-//   TextureLoader,
-//   Mesh,
-//   Shape,
-//   ShapeGeometry,
-// } from 'three';
-// import { BasisTextureLoader } from 'three/examples/jsm/loaders/BasisTextureLoader';
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
-// export const imgMap = {
-//   farm: { url: 'farm.png', data: null, loader: TextureLoader, obj3d: null },
-// };
-
-// export const preLoad = async () => {
-//   await Promise.all(
-//     Object.keys(imgMap).map((key) => {
-//       return new Promise((reslove, reject) => {
-//         const item = imgMap[key];
-//         if (item.loader) {
-//           const loader = new item.loader();
-//           loader.load(item.url, (data) => {
-//             item.data = data;
-//             const x = 0,
-//               y = 0,
-//               r = 10;
-//             const heartShape = new Shape();
-//             heartShape.moveTo(x, y - r);
-//             heartShape.lineTo(x - Math.sqrt(3) / 2, y - r / 2);
-//             heartShape.lineTo(x - Math.sqrt(3) / 2, y + r / 2);
-//             heartShape.lineTo(x, y + r);
-//             heartShape.lineTo(x + Math.sqrt(3) / 2, y + r / 2);
-//             heartShape.lineTo(x + Math.sqrt(3) / 2, y - r / 2);
-//             heartShape.lineTo(x, y - r);
-//             const geometry = new ShapeGeometry(heartShape);
-//             const material = new MeshBasicMaterial({
-//               map: data,
-//             });
-//             item.obj3d = new Mesh(geometry.rotateY(0), material);
-//           });
-//         }
-//       });
-//     }),
-//   );
-// };
 
 export const preLoadMapSource = async (map: Map) => {
   const imgMap: any = {
