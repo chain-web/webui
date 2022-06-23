00359e2e36d8d631ab38e95cc945b3f92baf100a
chengcheng <chengcheng@qcraft.ai>
2022-06-23 09:56:32 +0800

    use new did

--- src/page/test/accounts.ts
+++ src/page/test/accounts.ts
@@ -2,31 +2,43 @@ import { DidJson } from 'sk-chain';
 
 export const accounts: DidJson[] = [
   {
-    id: '12D3KooWL8qb3L8nKPjDtQmJU8jge5Qspsn6YLSBei9MsbTjJDr8',
+    id: 'SKw3o6cU3RRczeRJsJph84JR172a8mconNXhYvRLqHnjRWG3XPtY7j1Stv9ZdYQSPwbsTVJRsFgSZUnUtu1BwLtEiv53i',
     privKey:
-      'CAESQOu2tC61UCD6utWQpWndm8HSVWxi88P7cP29ydv6iHaOmVBTlFvfBXPpjZJeFi/Ult6HUOcVd9OOkyDg5TDibdk=',
+      'CAESQLADQxgC0anny3wNWtUjbNq3XjRjYhhLBC462W4pFK3Tfzl1btya4D/HltCIXyqn5AOd01MOWAbmW8TzKBNNXRg=',
+    pubKey: 'CAESIH85dW7cmuA/x5bQiF8qp+QDndNTDlgG5lvE8ygTTV0Y',
+    edPriv: 'b003431802d1a9e7cb7c0d5ad5236cdab75e346362184b042e3ad96e2914add3',
+    edPub: '7f39756edc9ae03fc796d0885f2aa7e4039dd3530e5806e65bc4f328134d5d18',
   },
   {
-    id: '12D3KooWL1NF6fdTJ9cucEuwvuX8V8KtpJZZnUE4umdLBuK15eUZ',
+    id: 'SKw3o7zKe4qsfTLKKC9U2FiB2TpWkFr9ZXBkmYKwhn6xW46ibf1dBkB9yUq43mZeP2C8py3WewowszdXbgNpod4nHNjYD',
     privKey:
-      'CAESQH+DR5TwHgEeKT/y/C8q3ixRVLOmIVeokvSmhZObWvrVl2ZX0c+MNcSLKy51EIwvS8y8gcVbIxmp0sJlztYRA1I=',
+      'CAESQPSNgfX+XKRXSWFjSfdgOgr1Fnw0J3hqEbaHM9ePYWCbxBamfQ6OTvDwxi+oVJTcwVOKfqb1AA0NCWeaVOXYXbM=',
+    pubKey: 'CAESIMQWpn0Ojk7w8MYvqFSU3MFTin6m9QANDQlnmlTl2F2z',
+    edPriv: 'f48d81f5fe5ca45749616349f7603a0af5167c3427786a11b68733d78f61609b',
+    edPub: 'c416a67d0e8e4ef0f0c62fa85494dcc1538a7ea6f5000d0d09679a54e5d85db3',
   },
   {
-    id: '12D3KooWHdhPrGCqsjD8j6yiHfumdzxfRxyYNPxJKN99RfgtoRuq',
+    id: 'SKw3o4vxNE4eE4sJBGjbipMxsqXoB3UTXzMhzmcwkZpVjwSpMhHjf5k1o113zxFhP6zRY29LjmL7tgARt9DVnzqKUkxG2',
     privKey:
-      'CAESQI/1ln/U9+4X+ovu4pb25U3jousyHSsR9YgXWi2/54DUdCHYam0uBGfydH5OZY1b70ihgEcYUKZTY5XkhtTEFZg=',
-    pubKey: 'CAESIHQh2GptLgRn8nR+TmWNW+9IoYBHGFCmU2OV5IbUxBWY',
+      'CAESQDD+9zvlryUX505Cc/7VBQACYUJ6TdlZfhCxiXkwGHI9KyJUdbgbqDT0ikMw/zaEUWvy95o86dKbGB4E+12H/dU=',
+    pubKey: 'CAESICsiVHW4G6g09IpDMP82hFFr8veaPOnSmxgeBPtdh/3V',
+    edPriv: '30fef73be5af2517e74e4273fed505000261427a4dd9597e10b189793018723d',
+    edPub: '2b225475b81ba834f48a4330ff3684516bf2f79a3ce9d29b181e04fb5d87fdd5',
   },
   {
-    id: '12D3KooWE4cb6m9DWsdsQh1gV8BzRdMhQkgeQcyGidDsug5uUoRz',
+    id: 'SKw3o6WxUPgpHgQDrtGDyeSZTjvAKPXK2qxqT9QjHueEQWGVxN5zyfgbnKpCTCe2kANAoJ5ELbMoemvrYzyygLAubwZuN',
     privKey:
-      'CAESQJuIaXd+gDMgfLaBJxXehmFqsAx2zGpPu2qdVBOJQtoMPxUvBprLeqShXIbZwVQ4QQJilXTqCqUcXCzsfPvHCdk=',
-    pubKey: 'CAESID8VLwaay3qkoVyG2cFUOEECYpV06gqlHFws7Hz7xwnZ',
+      'CAESQH5zbW47bGmXwp7/Q2uIAjrTgTplMWLjo9Fl+hnNSnt2enkV2eUGpzx6+tbRX9EWhVt7K3KFfn5WW6E82QbJjDQ=',
+    pubKey: 'CAESIHp5FdnlBqc8evrW0V/RFoVbeytyhX5+VluhPNkGyYw0',
+    edPriv: '7e736d6e3b6c6997c29eff436b88023ad3813a653162e3a3d165fa19cd4a7b76',
+    edPub: '7a7915d9e506a73c7afad6d15fd116855b7b2b72857e7e565ba13cd906c98c34',
   },
   {
-    id: '12D3KooWJHQ8kvM97Fz7i5CN3chfeRSrG51soNayhGHSeaqdDT5v',
+    id: 'SKw3o6UiE5D4MafAJE2Apjb2Qxgrh6G6cua5RpduRHmZKB233WBiiVFJ3W2GhSDwJdCvaJAfDWzorXyhDq3bK72dEVtcJ',
     privKey:
-      'CAESQAXiGJ17LnMgZrFCaMOoil1i289FG0drAFR5WrzSn2fffcpmYl6JkHXPcrjD2AcyLrHxiffG/4VntnY2wGwWnKU=',
-    pubKey: 'CAESIH3KZmJeiZB1z3K4w9gHMi6x8Yn3xv+FZ7Z2NsBsFpyl',
+      'CAESQEIjiROKeNayFpMKWJgZUce1k7IABg8FXmS1gzPz7/KBeIlUC0ZSpma5iV7YBpuGGY+FVMnPp7Lz4gmcUBeP+NI=',
+    pubKey: 'CAESIHiJVAtGUqZmuYle2AabhhmPhVTJz6ey8+IJnFAXj/jS',
+    edPriv: '422389138a78d6b216930a58981951c7b593b200060f055e64b58333f3eff281',
+    edPub: '7889540b4652a666b9895ed8069b86198f8554c9cfa7b2f3e2099c50178ff8d2',
   },
 ];
